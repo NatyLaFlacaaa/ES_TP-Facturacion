dbfccce3be67f27c9334cc317b889fa0008fb37a
NatyLaFlacaaa <nataliarodiciooo@gmail.com>
2024-07-06 16:18:09 -0300

    Add files via upload

---
+++ Ejercicio_B.py
@@ -0,0 +1,6 @@
+# Ejercicio B
+producto_1 = int(input("Por favor, ingresa el precio del primer producto: "))
+producto_2 = int(input("Ahora el precio del segundo: "))
+producto_3 = int(input("Finalmente, el del tercer producto: "))
+promedio = (producto_1 + producto_2 + producto_3) / 3
+print("El promedio de tu compra es: $" + str(promedio))
---
+++ Ejercicio_C.py
@@ -0,0 +1,7 @@
+# Ejercicio C
+producto_1 = int(input("Por favor, ingresa el precio del primer producto: "))
+producto_2 = int(input("Ahora el precio del segundo: "))
+producto_3 = int(input("Finalmente, el del tercer producto: "))
+suma = producto_1 + producto_2 + producto_3
+IVA = 1.21
+print("El total de tu compra + IVA es: $" + str(suma * IVA))
